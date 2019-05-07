946933f6a79279a2e6b1a0cbe94b65887e413baf
kiranshenoy <kiranshenoy@Admins-MacBook-Air.local>
2019-05-07 19:12:27 +0530

    Adding Ui for add Contact Screen

--- app/src/main/java/com/gonuclei/contacts/ui/controller/ContactActivityController.java
+++ app/src/main/java/com/gonuclei/contacts/ui/controller/ContactActivityController.java
@@ -7,7 +7,6 @@ import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
