5f13e34573669397ba884f91556db2614fd74e53
kiranshenoy <kiranshenoy@Admins-MacBook-Air.local>
2019-05-07 17:07:17 +0530

    Adding Contact photo

--- app/src/main/java/com/gonuclei/contacts/adapter/ContactAdapter.java
+++ app/src/main/java/com/gonuclei/contacts/adapter/ContactAdapter.java
@@ -1,6 +1,7 @@
 package com.gonuclei.contacts.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
 import com.gonuclei.contacts.R;
 import com.gonuclei.contacts.model.Contact;
 
@@ -21,6 +24,7 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.Contacts
 
     private List<Contact> contactList;
     private Context context;
+    ColorGenerator generator = ColorGenerator.MATERIAL;
 
     public ContactAdapter(Context context, List<Contact> contactList) {
         this.context = context;
@@ -37,6 +41,9 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.Contacts
     @Override
     public void onBindViewHolder(@NonNull ContactsViewHolder holder, int position) {
         holder.mContactNameView.setText(contactList.get(position).getDisplayName());
+        TextDrawable drawable = TextDrawable.builder()
+                .buildRound(contactList.get(position).getDisplayName().substring(0,1), generator.getRandomColor());
+        holder.mContactImageView.setImageDrawable(drawable);
         holder.mContactLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
